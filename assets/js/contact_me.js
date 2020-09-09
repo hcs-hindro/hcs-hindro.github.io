aa983667185f9097e892de09ba1124e10c5c76ab
hcs-hindro <70940496+hcs-hindro@users.noreply.github.com>
2020-09-09 18:34:03 +0530

    Updated error message to expose email

--- assets/js/contact_me.js
+++ assets/js/contact_me.js
@@ -99,15 +99,24 @@ $(function() {
           $('#contactForm').trigger("reset");
         },
 		
-        error: function (jqXHR, textStatus, errorThrown) {
+        error: function () {
+          $('#success').html("<div class='alert alert-success'>");
+          $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+            .append("</button>");
+          $('#success > .alert-success')
+            .append("<strong>Thank you! If you don't hear back from us within a week, please feel free to drop a mail at anshu@hindro.in </strong>");
+          $('#success > .alert-success')
+            .append('</div>');
+          //clear all fields
+          $('#contactForm').trigger("reset");
             // Fail message
-            $('#success').html("<div class='alert alert-danger'>");
-            $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-              .append("</button>");
-            $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
-            $('#success > .alert-danger').append('</div>');
-            //clear all fields
-            $('#contactForm').trigger("reset");
+            // $('#success').html("<div class='alert alert-danger'>");
+            // $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+            //   .append("</button>");
+            // $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
+            // $('#success > .alert-danger').append('</div>');
+            // //clear all fields
+            // $('#contactForm').trigger("reset");
         },
 		
         complete: function() {
